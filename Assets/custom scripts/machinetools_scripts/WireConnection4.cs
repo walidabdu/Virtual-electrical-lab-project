f04d2a830be38d9d97fcf74b52d04366c72e73c8
walidabdu <136993178+walidabdu@users.noreply.github.com>
2025-05-17 14:49:59 +0300

    fixed lighting issue and changed from wire connection to wire connection4

---
+++ Assets/custom scripts/machinetools_scripts/WireConnection4.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class WireConnection4 : MonoBehaviour
+{
+    public bool isConnected = false;
+    public GameObject connectedWire; // The actual wire GameObject plugged in
+    public CircuitManager circuitManager;
+    private XRSocketInteractor socketInteractor;
+
+    private void Awake()
+    {
+        socketInteractor = GetComponent<XRSocketInteractor>();
+        socketInteractor.selectEntered.AddListener(OnWireConnected);
+        socketInteractor.selectExited.AddListener(OnWireDisconnected);
+    }
+
+    private void OnWireConnected(SelectEnterEventArgs args)
+    {
+        isConnected = true;
+        connectedWire = args.interactableObject.transform.gameObject;
+        if (circuitManager != null) circuitManager.CheckCircuit();
+    }
+
+    private void OnWireDisconnected(SelectExitEventArgs args)
+    {
+        isConnected = false;
+        connectedWire = null;
+        if (circuitManager != null) circuitManager.CheckCircuit();
+    }
+}
