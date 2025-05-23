0f8827e133449095832009374203a978fa7721f6
walidabdu <136993178+walidabdu@users.noreply.github.com>
2025-05-23 10:34:33 +0300

    modified wireconnection4 to return both the name of the socket or node and also the connected wire pin

--- Assets/custom scripts/machinetools_scripts/WireConnection4.cs
+++ Assets/custom scripts/machinetools_scripts/WireConnection4.cs
@@ -8,6 +8,12 @@ public class WireConnection4 : MonoBehaviour
     public CircuitManager circuitManager;
     private XRSocketInteractor socketInteractor;
 
+    // Public property to get the socket's GameObject name
+    public string SocketName => gameObject.name;
+
+    // Public property to get the connected wire's name (or null if not connected)
+    public string ConnectedWireName => isConnected ? connectedWire.name : null;
+
     private void Awake()
     {
         socketInteractor = GetComponent<XRSocketInteractor>();
@@ -19,13 +25,31 @@ public class WireConnection4 : MonoBehaviour
     {
         isConnected = true;
         connectedWire = args.interactableObject.transform.gameObject;
+        
+        // Debug log to show both names when connected
+        Debug.Log($"Wire connected: Socket '{SocketName}' received wire '{ConnectedWireName}'");
+        
         if (circuitManager != null) circuitManager.CheckCircuit();
     }
 
     private void OnWireDisconnected(SelectExitEventArgs args)
     {
+        // Debug log to show both names when disconnected
+        if (isConnected)
+        {
+            Debug.Log($"Wire disconnected: Socket '{SocketName}' lost wire '{ConnectedWireName}'");
+        }
+        
         isConnected = false;
         connectedWire = null;
         if (circuitManager != null) circuitManager.CheckCircuit();
+    }
+
+    // Helper method to get connection info
+    public string GetConnectionInfo()
+    {
+        return isConnected 
+            ? $"Socket '{SocketName}' is connected to wire '{ConnectedWireName}'" 
+            : $"Socket '{SocketName}' is not connected to any wire";
     }
 }
